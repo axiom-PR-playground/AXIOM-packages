4046cf1658ec0316592eabd230f2767f14018357
MedicineYeh <medicinehy@gmail.com>
2018-11-22 22:39:20 +0800

    Modify build script for better functionality in auto-build env, i.e. CI

--- build.sh
+++ build.sh
@@ -2,6 +2,23 @@
 
 REPOS=$(ls repo)
 
-for repo in $REPOS; do
-    (cd "repo/${repo}" && ../../makepkg.sh)
-done
+# Usage: ./build.sh [repo]
+#  If repo is specified, build the specific repo.
+#  Otherwise, build all packages.
+
+if [[ "$1" != "" ]]; then
+    if [[ -d "$1" ]]; then
+        (cd "$1" && ../../makepkg.sh)
+    else
+        echo "$1 is not a directory" && exit 1
+    fi
+else
+    for repo in $REPOS; do
+        (cd "repo/${repo}" && ../../makepkg.sh)
+    done
+fi
+
+rm -f apertus.db  apertus.db.tar.gz  apertus.files  apertus.files.tar.gz
+repo-add apertus.db.tar.gz $(find ./repo -name '*.pkg.tar.xz')
+
+exit 0
